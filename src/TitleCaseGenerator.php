22b8f7a27d391ba91d7d38d7dd6491111993a160
John Franti & Patrick Fiorentino <student@epicodus.com>
2015-03-09 15:17:07 -0700

    Remove iTunes type exclusions.

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -4,10 +4,9 @@ class TitleCaseGenerator
 {
     function makeTitleCase($input_title)
     {
-        $lowered_title = strtolower($input_title);
         $input_array_of_words = explode(" ", $lowered_title);
         $output_titlecased = array();
-        $exclude = array("of", "the", "and", "as", "a", "in", "into", "an", "if", "iPod", "iTunes");
+        $exclude = array("of", "the", "and", "as", "a", "in", "into", "an", "if");
         $first_word = $input_array_of_words[0];
         $last_word = end($input_array_of_words);
 
