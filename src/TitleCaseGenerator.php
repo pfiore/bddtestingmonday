0653984e80e8d70ef04babb9e4d5b00e843e2d73
John Franti & Patrick Fiorentino <student@epicodus.com>
2015-03-09 12:20:41 -0700

    Implement Spec 3.

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -2,12 +2,19 @@
 
 class TitleCaseGenerator
 {
+
     function makeTitleCase($input_title)
     {
         $input_array_of_words = explode(" ", $input_title);
         $output_titlecased = array();
+        $exclude = array("of", "the", "and", "as", "a", "an", "if");
         foreach ($input_array_of_words as $word) {
-            array_push($output_titlecased, ucfirst($word));
+            if (in_array($word, $exclude)) {
+                array_push($output_titlecased, $word);
+            }
+            else {
+                array_push($output_titlecased, ucfirst($word));
+            }
         }
         return implode(" ", $output_titlecased);
     }
